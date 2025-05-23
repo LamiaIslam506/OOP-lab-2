fdb1a0ae0e5ef44689927b07863213cb7b52f1e9
LamiaIslam506 <lamia241-15-506@diu.edu.bd>
2025-05-23 19:27:04 +0600

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Circle {
+
+     float radius;
+     float PI = 3.1416f;
+     void area () {
+            System.out.println("Area is " + PI * radius * radius);
+        }
+
+}
